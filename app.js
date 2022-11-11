3a33f0a33167100e2ec8c0f24081dc31fcacd355
Israel <93683580+blueishegg@users.noreply.github.com>
2022-11-11 11:51:04 +0000

    cors installed to allow access locally

--- app.js
+++ app.js
@@ -3,12 +3,16 @@ import express from "express";
 import path from "path";
 import cookieParser from "cookie-parser";
 import logger from "morgan";
-
+import cors from "cors";
 // import indexRouter from "./routes/index.js";
 import usersRouter from "./routes/users.js";
 
 var app = express();
-
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+  })
+);
 // view engine setup
 // app.set("views", path.join(__dirname, "views"));
 // app.set("view engine", "jade");
